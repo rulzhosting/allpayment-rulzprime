363050d095fb88c40a0419fc51357fad72f878ca
rulzhosting <webp1120@gmail.com>
2025-06-13 00:28:10 +0700

    Update main.js

--- main.js
+++ main.js
@@ -90,7 +90,7 @@
           
           document.querySelector('.payment-option[data-type="qris"]').classList.add('selected', 'pulse');
         }
-        }
+
         // Process payment function
         function processPayment() {
   const url = 'https://wa.me/6283137210530';
